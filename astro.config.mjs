079b4b5e78d83f797601c0c5d0a97d7444d729ca
Clément Rioche <riocheclement@gmail.com>
2024-10-30 10:13:08 +0100

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,30 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
+
+import partytown from "@astrojs/partytown";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://RiocheClement.github.io",
+  image: {
+    domains: ["raw.githubusercontent.com"],
+  },
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
+});
---
+++ readme.md
@@ -0,0 +1,7 @@
+# website
+
+This the code for my personal portfolio.
+Thanks to Astro and Naya Singhania
+
+1. `npm run dev`
+2. Go to http://localhost:4321
